9dafc0104b84a2beb5db40e54b9a5b6896631e98
qwertyuiopp <xuyinyui@gmail.com>
2026-06-14 13:36:32 +0800

    w

--- packages/compute-node/main.go
+++ packages/compute-node/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"compute-node/packages/SearchEngine"
-	"compute-node/packages/UnitTest"
-)
-
 func main() {
-	UnitTest.RunEventEngineTests()
-	SearchEngine.Example()
+
 }
